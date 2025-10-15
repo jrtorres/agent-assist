f788344377b051810ca4f4c908aba3a5e2993e46
Javier Torres <jrtorres@us.ibm.com>
2025-10-15 09:18:19 -0400

    improved summarization prompt for llama model

--- celery/aan_extensions/SummaryAgent/summ.py
+++ celery/aan_extensions/SummaryAgent/summ.py
@@ -1,14 +1,13 @@
 # Import necessary libraries
-import ibm_watson_machine_learning
 from ibm_watson_machine_learning.foundation_models import Model
 from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
 # import textwrap
 # import nltk
-import os
 # from nltk.tokenize import sent_tokenize, word_tokenize
 # from collections import Counter
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 
@@ -26,9 +25,12 @@ DETAIL = 0.5
 MAX_NEW_TOKENS = 500
 TOKEN_LIMIT = 1024
 # Initialize the model
-generate_params = {GenParams.MAX_NEW_TOKENS: MAX_NEW_TOKENS}
+generate_params = {GenParams.MAX_NEW_TOKENS: MAX_NEW_TOKENS, 
+                   GenParams.DECODING_METHOD: "greedy",
+                   GenParams.REPETITION_PENALTY: 1.2}
+
 model_name = os.environ.get('AAN_SUMMARIZATION_LLM_MODEL_NAME', 'ibm/granite-13b-chat-v2')
-logging.info(f'LLM model name to be used: {model_name}')
+logging.info(f'LLM model name to be used for summarization: {model_name}')
 model = Model(
     model_id=model_name,
     params=generate_params,
@@ -47,22 +49,32 @@ def summarize_text(segment):
     :return: The summary of the segment as a single paragraph.
     """
     try:
-        system_prompt = "<<SYS>>You are a concise and efficient summarizer. " \
-                        "Generate a summary that condenses the conversation into a single paragraph, " \
-                        "focusing on main points, emotions, and outcomes. " \
-                        "Avoid itemizing the dialogue or adding new information. " \
-                        "Ensure the summary is coherent, neutral, and unbiased.<</SYS>>"
+        if model_name == 'meta-llama/llama-3-3-70b-instruct':
+            summarization_prompt = """ 
+                <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+                You are a helpful assistant with the task of precisely summarizing the conversation text between an agent and a user. 
+                Be concise and direct, avoiding additional context or narrative. Ensure the summary is coherent, neutral, and accurate.
+                <|eot_id|><|start_header_id|>user<|end_header_id|>Summarize the following conversation between an agent and a user into a concise paragraph.
+                Conversation Text: 
+                {0}
+
+                Summary:
+                <|eot_id|><|start_header_id|>assistant<|end_header_id|>""".format(segment)
+        else:
+            summarization_prompt = """ 
+                You are a helpful, respectful and honest assistant in the financial services industry. 
+                Your primary goal is to summarize a conversation between a call center agent and a customer. In the conversation, 
+                the agent utterances are indicated by the label 'Agent:' and the customer utterances are indicated by the label 'Customer:'. 
+                The summary should be a concise paragraph. Do not generate additional utterances for the conversation or restate the conversation.
+                
+                {0}
+
+                Summary:
+                """.format(segment)
         
-        instruction_prompt = f"{segment}"
-
-        full_prompt = f"<s>[INST]{system_prompt}\n\n{instruction_prompt}[/INST]</s>"
-
-        summary_response = model.generate_text(prompt=full_prompt)
-
+        summary_response = model.generate_text(prompt=summarization_prompt)
         summary_text = summary_response if isinstance(summary_response, str) else summary_response.get('generated_text', '')
-
         summary_text = ' '.join(summary_text.splitlines())
-
         return summary_text
     except Exception as e:
         logging.error(f"Error during summarization: {e}")
@@ -96,7 +108,9 @@ def summarize(transcript):
     combined_summary = summarize_text(transcript)
 
     if len(combined_summary.split()) > 80:
-        combined_summary = summarize_text(combined_summary)
+        logging.warning("The summary is long.")
+        # JRT - Create new summarization prompt to condense the summary
+        #combined_summary = summarize_text(combined_summary)
 
     # if len(combined_summary.split()) < 30:
     #     logging.warning("The summary may be too concise. Consider adjusting the segmenting or summarization logic.")
